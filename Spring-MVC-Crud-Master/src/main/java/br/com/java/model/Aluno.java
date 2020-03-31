273efd63d47bac13fe4916a3f09db9dad2fae416
financeiro06 <financeiro06@192.168.56.1>
2020-03-31 17:18:19 -0300

    criação da classe aluno

---
+++ Spring-MVC-Crud-Master/src/main/java/br/com/java/model/Aluno.java
@@ -0,0 +1,67 @@
+package br.com.java.model;
+
+public class Aluno {
+	
+    private int cpf;
+	
+	private String nome;
+	
+	private String curso;
+	
+	private String endereco;
+	
+	private String telefone;
+	
+	private Escola escola;
+	
+    public Aluno() {}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Escola getEscola() {
+		return escola;
+	}
+
+	public void setEscola(Escola escola) {
+		this.escola = escola;
+	}
+		
+}
